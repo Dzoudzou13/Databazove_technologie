5feb02e0cef17535b74fbf78d3d69f3d6b981466
Jozef Kušnierik <jozfkusnierik2@gmail.com>
2026-09-23 14:26:51 +0200

    TASK 8: Count orders for each customer

---
+++ cvicenia_8.sql
@@ -0,0 +1,7 @@
+SELECT
+    c.customer_name,
+    COUNT(o.order_id) AS order_count
+FROM customers AS c
+LEFT JOIN orders AS o
+    ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.customer_name;
